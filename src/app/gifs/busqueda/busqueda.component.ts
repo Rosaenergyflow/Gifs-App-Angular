46975c623f082d528134b1589aa93aeb7f223de7
Rosa Maria BJ <62024358+Rosaenergyflow@users.noreply.github.com>
2021-09-29 16:59:01 +0200

    viewchild

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent{
+  
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  buscar() {
+    var valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
+    this.txtBuscar.nativeElement.value = '';
+  }
+
 }
